1c533cffb248fdffcad1ddfdcec125adebc4edd4
Gideon-Muriithi <gideongakenge@gmail.com>
2019-08-23 17:47:47 +0300

    This commit adds the application to github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
