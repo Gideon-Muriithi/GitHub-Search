8c942dd22093002870b60d54f74ba4282e5c8b2a
Gideon-Muriithi <gideongakenge@gmail.com>
2019-08-25 20:16:39 +0300

    This commit adds fucntionalities in the profile service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,20 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { ProfileService } from './services/profile.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    HttpClientModule
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule,
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private username: string;
+  private clientid = 'Iv1.9584babc76dba06d';
+  private clientsecret = '00b5717c619098aafc0945c76a6030bb31590cd8';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    console.log('Service is now ready!');
+    this.username = 'Gideon-Muriithi';
+  }
+
+  getProfileInfo() {
+    return this.http.get('https://api.github.com/users' + this.username + '?client_id=' + this.
+   clientid + '&client-secret=' + this.clientsecret)
+   .map(res => res.json());
+  }
 }
